c902ea071a1f6eff415569c1af203cc98ef67f39
Zizo <zizo@yahoo.com>
2022-11-24 10:26:55 +0200

    ok

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
     res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+    res.setHeader("Content-Type", "application/json")
     next()
 })
 app.use(router)
